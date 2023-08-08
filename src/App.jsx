211bd45ad37fde7ed483838e00a32d9ed5757e5a
Anjali <anjalitiwarijpguna@gmail.com>
2023-08-08 10:37:39 +0530

    all the updates

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+import MainLayout from './components/MainLayout';
+import Show from './pages/Show';
+import { GlobalTheme } from './theme';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GlobalTheme>
+        <HashRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/starred" element={<Starred />} />
+            </Route>
+
+            <Route path="/show/:showId" element={<Show />} />
+            <Route path="*" element={<div>Not Found</div>} />
+
+            {/* <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route>
+      </Route>
+      <Route element={<PageLayout />}>
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/tos" element={<Tos />} />
+      </Route>
+  <Route path="contact-us" element={<Contact />} /> */}
+          </Routes>
+        </HashRouter>
+      </GlobalTheme>
+    </QueryClientProvider>
+  );
+}
+export default App;
